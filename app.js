369c69d064cf3563efba03e65775e41f912c82e8
Timilehin ebenezer Adedara <adedaratimilehin.gold@gmail.com>
2023-09-08 19:07:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+const d = new Date().getDay();
+
+const now = new Date();
+
+
+
+
+
+document.getElementById('day').innerHTML = days[d];
+document.getElementById('time').innerHTML = month;    
+   
+
